ce980cfd6d2261dbfa19cc1834beb5d265f4635e
joan lovelle alfeche <jlovellealfeche@gmail.com>
2023-07-05 15:50:27 +0200

    update html

---
+++ index.js
@@ -0,0 +1,15 @@
+function rate(rating) {
+    // Display the selected rating
+    document.getElementById('result').textContent = "You rated: " + rating + " stars";
+  
+    // Update the star colors based on the selected rating
+    var stars = document.getElementsByClassName('star');
+    for (var i = 0; i < stars.length; i++) {
+      if (i < rating) {
+        stars[i].style.backgroundColor = 'yellow';
+      } else {
+        stars[i].style.backgroundColor = 'white';
+      }
+    }
+  }
+  
